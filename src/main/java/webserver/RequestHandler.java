50231cf1d8ed8fbc4741df971df3385170ec4b4a
jayden-lee <gwanggi.lee@gmail.com>
2022-12-15 01:01:21 +0900

    Get 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,14 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,7 +34,22 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
 
-            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+            String url = tokens[1];
+            String requestPath = "/index.html";
+            if (url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                Map<String, String> params = HttpRequestUtils.parseQueryString(url.substring(index + 1));
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("Create user: {}", user);
+
+            } else if (url.equals("/")) {
+                requestPath = "/index.html";
+
+            } else {
+                requestPath = url;
+            }
+
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
             DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
